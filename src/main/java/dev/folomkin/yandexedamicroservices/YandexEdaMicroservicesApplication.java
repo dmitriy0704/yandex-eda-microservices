8fddbdb68eed7a3c23c664bd5a3626d7b92a34ca
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2025-12-29 13:33:16 +0300

    init app

---
+++ src/main/java/dev/folomkin/yandexedamicroservices/YandexEdaMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package dev.folomkin.yandexedamicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexEdaMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YandexEdaMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/folomkin/yandexedamicroservices/YandexEdaMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.folomkin.yandexedamicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YandexEdaMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
